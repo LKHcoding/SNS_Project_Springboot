c3692490a13bf0d1dc930b2d3c00010eb5f1ea9e
whowho0820 <whowho0820@naver.com>
2020-10-06 03:55:52 -0400

    내정보화면 팔로워클릭 시 모달창 생성 작업중

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,16 +3,40 @@ $(document).ready(function() {
     $overlay = $(".profile__overlay"),
     $closeBtn = $(".fa-times"),
     $cancelBtn = $("#cancel"),
-    $logoutBtn = $("#logout");
+    $logoutBtn = $("#logout"); 
+
+
+    $follower = $(".followerClick"),
+    $overlay1 = $(".profile__overlay1"),
+    $closeBtn1 = $(".fa-times"),
+    $cancelBtn1 = $("#cancel"),
+    $logoutBtn1 = $("#logout"); 
+
+
+  $follower.click(function() {
+    $overlay1.fadeIn();
+  });
+
+  $closeBtn1.click(closePopup);
+  $cancelBtn1.click(closePopup);
+
+   function closePopup() {
+    $overlay1.fadeOut();
+  }
+
+  $logoutBtn1.click(function() {
+    closePopup();
+  });
 
   $cog.click(function() {
     $overlay.fadeIn();
   });
 
+
   $closeBtn.click(closePopup);
   $cancelBtn.click(closePopup);
 
-  function closePopup() {
+   function closePopup() {
     $overlay.fadeOut();
   }
 
@@ -20,3 +44,4 @@ $(document).ready(function() {
     closePopup();
   });
 });
+
