5ea8dbb8161fb74081fce022de96e216ef27f2fe
ParkYoungUn <youngun45@naver.com>
2020-10-16 10:59:47 +0900

    feed에 자기글이 안뜨는 현상 수정 및 새글 맨 위로 올라가도록 구현

--- src/main/java/com/cos/instagram/domain/image/ImageRepository.java
+++ src/main/java/com/cos/instagram/domain/image/ImageRepository.java
@@ -6,16 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ImageRepository extends JpaRepository<Image, Integer>{
-	
+
 	List<Image> findByUserId(int userId);
-	
+
 	// 내가 팔로우 하지 않은 사람들의 이미지들(최대 20개)
 	@Query(value = "select * from image where userId in (select id from user where id != ?1 and id not in (select toUserId from follow where fromUserId = ?1)) limit 20", nativeQuery = true)
 	List<Image> mNonFollowImage(int loginUserId);
-	
-	@Query(value="select * from image where userId in (select toUserId from follow where fromUserId = ?1)", nativeQuery = true)
+
+	@Query(value="select * from image where userId = ?1 or userId in (select toUserId from follow where fromUserId = ?1) order by createDate desc", nativeQuery = true)
 	List<Image> mFeeds(int loginUserId);
-	
+
 	@Query(value="select * from image where id in (select imageId from tag where name=?1)", nativeQuery = true)
 	List<Image> mFeeds(String tag);
 }
