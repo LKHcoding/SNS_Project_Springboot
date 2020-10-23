ffd064d86dd380eb8234fdbddc3e3274aed41898
ParkYoungUn <youngun45@naver.com>
2020-10-23 16:27:21 +0900

    비밀번호 수정기능 구현 (다른사람 비밀번호 수정할 수 있음 잡아야됨) localhost:8080/auth/pwChange 에서 수정가능

--- src/main/java/com/cos/instagram/domain/user/UserRepository.java
+++ src/main/java/com/cos/instagram/domain/user/UserRepository.java
@@ -1,9 +1,12 @@
-﻿package com.cos.instagram.domain.user;
+package com.cos.instagram.domain.user;
 
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 // JpaRepository가 extends 되면 @Repository 필요 없음. IoC자동으로 됨.
@@ -24,4 +27,14 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	// 회원가입시 이메일, username 중복체크하는 부분
 	@Query(value = "select * from user where email = ?1 or username = ?2", nativeQuery = true)
 	List<User> 중복체크(String email, String username);
+
+	// 비밀번호 변경
+	@Modifying(clearAutomatically = true)
+	@Transactional  // delete 사용시
+	@Query(value = "update user set password = ?2 where username = ?1", nativeQuery = true)
+	int modifyPassword(String username, String password);
+//	List<JoinReqDto> modifyPassword(String username,String password);
+//	int modifyPassword(int username, int password);
+
+	//void modifyPassword(JoinReqDto joinReqDto) throws Exception;
 }
--- src/main/java/com/cos/instagram/service/UserService.java
+++ src/main/java/com/cos/instagram/service/UserService.java
@@ -88,6 +88,28 @@ public class UserService {
 		userEntity.setGender(user.getGender());
 	}
 
+	@Transactional(readOnly = false)
+	public void 비밀번호변경(JoinReqDto joinReqDto) throws Exception{
+
+		String username = joinReqDto.getUsername();
+		LoginUser loginUser;
+		System.out.println("비밀번호 변경 서비스 진입");
+		//String encPassword = bCryptPasswordEncoder.encode(joinReqDto.getPassword());
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println("username: "+joinReqDto.getUsername());
+		System.out.println("암호화 전 비밀번호: "+joinReqDto.getPassword());
+
+		//비밀번호를 암호화하여 joinReqDto객체에 다시 저장
+		String newPassword = encoder.encode(joinReqDto.getPassword());
+		joinReqDto.setPassword(newPassword);
+
+		System.out.println("암호화 후 비밀번호  : " + joinReqDto.getPassword());
+//		joinReqDto.setPassword(encPassword);
+
+		userRepository.modifyPassword(username,newPassword);
+//		userRepository.save(joinReqDto.toEntity());
+	}
+
 	@Transactional(readOnly = true)
 	public User 회원정보(LoginUser loginUser) {
 		return userRepository.findById(loginUser.getId()).orElseThrow(new Supplier<MyUserIdNotFoundException>() {
